bb0c4e735aa9740622fd2f01be234e3bd299ae0e
Michael Nutt <michael@nuttnet.net>
2021-08-03 14:14:06 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require './ps'
+require 'tempfile'
+
+def get_processes(search)
+  Ps.all.select{|i| i[:command] =~ /#{search}/ && !i[:command].start_with?('[') }
+end
+
+get '/' do
+  @processes = get_processes(ENV['PROC_PATTERN'])
+
+  erb :index
+end
+
+post '/profile/:pid' do
+  referer = request.env['HTTP_REFERER']
+  referer_host = URI.parse(referer).host
+
+  return "BAD REQUEST" unless referer_host == "localhost"
+
+  pid = params[:pid].to_i
+  process = get_processes(ENV['PROC_PATTERN']).find{|process| process[:pid].to_i == pid }
+  return "BAD PID" unless process
+
+  sample_time = (params[:sample_time] || 20).to_i
+  sample_rate = (params[:sample_rate] || 99).to_i
+
+  if sample_time <= 0 || sample_rate <= 0 || sample_time > 120 || sample_rate > 999
+    return "BAD REQUEST"
+  end
+
+  data_path = Tempfile.new.path
+  puts "Writing to: #{data_path}"
+
+  puts `sudo perf record -p #{pid} -o "#{data_path}" -F #{sample_rate} --call-graph dwarf -g -- sleep #{sample_time}`
+
+  perf_path = Tempfile.new.path
+  puts `perf script -i "#{data_path}" > "#{perf_path}"`
+
+  folded_path = Tempfile.new.path
+  puts `./flame-graph/stackcollapse-perf.pl "#{perf_path}" > "#{folded_path}"`
+
+  svg = `./flame-graph/flamegraph.pl "#{folded_path}"`.chomp
+
+  content_type :svg
+  svg
+end
---
+++ ps.rb
@@ -0,0 +1,52 @@
+# Ps.all to get an array of hashes
+# Example hash:
+# {
+#   :user=>"Username",
+#   :pid=>5321.0,
+#   :cpu=>6.2,
+#   :mem=>0.7,
+#   :vsz=>3438156.0,
+#   :rss=>56568.0,
+#   :tt=>"??",
+#   :stat=>"S",
+#   :started=>"12:59pm",
+#   :time=>"14:33.15",
+#   :command=>"/Applications/Google"
+# }
+class Ps
+  class << self
+    def user
+      parse(ps_aux)
+    end
+
+    def all
+      parse(ps_aux)
+    end
+
+    private
+
+    def parse(str)
+      processes = str.split /\r?\n/
+      processes.shift
+      processes.map do |process_str|
+        p_obj = {}
+        p_arr = process_str.split ' '
+        sym_arr = [:user, :pid, :cpu, :mem, :vsz, :rss, :tt, :stat, :started, :time, :command]
+        sym_arr.each_with_index do |sym, i|
+          begin
+            p_obj[sym] = Float(p_arr[i])
+          rescue
+            p_obj[sym] = p_arr[i]
+          end
+        end
+
+        p_obj
+      end
+    end
+
+    def ps_aux
+      `ps aux`
+    end
+  end
+end
+
